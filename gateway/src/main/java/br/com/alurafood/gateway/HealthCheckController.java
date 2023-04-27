f18d1db991925398c244b9572905259295702101
welisson <welisson.oliveiraa@gmail.com>
2023-04-27 10:34:02 -0300

    arquivos de configuracao do projeto no kubernates

---
+++ gateway/src/main/java/br/com/alurafood/gateway/HealthCheckController.java
@@ -0,0 +1,16 @@
+package br.com.alurafood.gateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health-check")
+public class HealthCheckController {
+
+    @GetMapping
+    public boolean healthCheck() {
+        return true;
+    }
+
+}
